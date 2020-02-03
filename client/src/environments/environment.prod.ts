79739291b96e471171a4c8c85b4ceed91b0236d9
Antonio <info@antoniopicone.it>
2020-02-04 00:07:58 +0100

    Fixes to build prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: "AIzaSyABoDPZ-Nkdvttkw7NWfFRsRWm5I4fRVck",
+    authDomain: "is2-wishlist-project.firebaseapp.com",
+    databaseURL: "https://is2-wishlist-project.firebaseio.com",
+    projectId: "is2-wishlist-project",
+    storageBucket: "is2-wishlist-project.appspot.com",
+    messagingSenderId: "381186058880",
+    appId: "1:381186058880:web:6bc03bb425e2a07f45d465"
+  }
 };
 
 export const SERVER_URL = 'https://us-central1-is2-wishlist-project.cloudfunctions.net/mockServer';
