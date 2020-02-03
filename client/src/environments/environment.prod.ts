26fbdad71a9301d3ac27cd939c2b1543ba549eaf
Antonio <info@antoniopicone.it>
2020-02-03 17:36:14 +0100

    Testing deploy to Firebase

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const SERVER_URL = 'http://127.0.0.1:3000';
+export const SERVER_URL = 'https://us-central1-is2-wishlist-project.cloudfunctions.net/mockServer';
