359c539e1f0749253c89f38cc3a0509e3c8771b7
Antonio <antonio@MacBook-Pro-di-Antonio.local>
2020-02-20 00:58:16 +0100

    Updated dir structure and script.sh

--- tests/2.1/REGRESSION TEST/script.sh
+++ tests/2.1/REGRESSION TEST/script.sh
@@ -1,5 +1,15 @@
+echo "####### PROCESSO BASE #######"
+cd ProcessoBase
 for f in *.py; do
 echo "####### $f ######"
 python "$f" >> test_log.txt;
 done
+cd ..
+echo "####### PROCESSO MIGLIORATO #######"
+cd ProcessoMigliorato
+for f in *.py; do
+echo "####### $f ######"
+python "$f" >> test_log.txt;
+done
+
 read -p "Press [Enter] key to exit..."
