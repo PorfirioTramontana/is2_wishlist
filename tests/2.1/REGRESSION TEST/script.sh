57a518a60cac38c165721fc365747f6588bb0842
Antonio <antonio@MacBook-Pro-di-Antonio.local>
2020-02-20 01:10:07 +0100

    Updated script.sh, annotation for db restore

--- tests/2.1/REGRESSION TEST/script.sh
+++ tests/2.1/REGRESSION TEST/script.sh
@@ -1,15 +1,17 @@
 echo "####### PROCESSO BASE #######"
 cd ProcessoBase
 for f in *.py; do
+# restore database.json
 echo "####### $f ######"
-python "$f" >> test_log.txt;
+python "$f" >> ../log.txt;
 done
 cd ..
 echo "####### PROCESSO MIGLIORATO #######"
 cd ProcessoMigliorato
 for f in *.py; do
+# restore database.json
 echo "####### $f ######"
-python "$f" >> test_log.txt;
+python "$f" >> ../log.txt;
 done
 
 read -p "Press [Enter] key to exit..."
